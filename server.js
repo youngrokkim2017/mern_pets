28ee8d2b2f072abe39adb06a76e90a27fa31e03b
Patrick Kim <patrick.yrkim@gmail.com>
2022-08-25 23:32:28 -0700

    Add public folder

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 app.set("view engine", "ejs")
 app.set("views", "./views")
 
+app.use(express.static("public"))
+
 app.get("/", async (req, res) => {
     const allAnimals = await db.collection("animals").find().toArray()
     console.log(allAnimals)
