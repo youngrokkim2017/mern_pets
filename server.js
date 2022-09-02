95fc8ea7132ea9181042bc53880ec217b8a9a1b2
Patrick Kim <patrick.yrkim@gmail.com>
2022-09-02 00:00:16 -0700

    Add middleware for password protected routes

--- server.js
+++ server.js
@@ -8,12 +8,26 @@ app.set("views", "./views")
 
 app.use(express.static("public"))
 
+function passwordProtected(req, res, next) {
+    res.set("WWW-Authenticate", "Basic realm='Our MERN App'")
+    if (req.headers.authorization == "Basic YWRtaW46YWRtaW4=") {
+        next()
+    } else {
+        console.log(req.headers.authorization)
+       res.status(401).send("try again")
+    }
+}
+
 app.get("/", async (req, res) => {
     const allAnimals = await db.collection("animals").find().toArray()
     console.log(allAnimals)
     // res.send("Welcome to home page")
     res.render("home", {allAnimals})
 })
+
+// any urls after '/' is password protected
+// ordering matters
+app.use(passwordProtected)
 
 app.get("/admin", (req, res) => {
     // res.send("This is the admin page")
